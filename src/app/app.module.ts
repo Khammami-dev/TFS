c96421a31b488372feb8ed415e2d5ae992f1b1ba
Achref Soltani <60237898+Ach-So@users.noreply.github.com>
2021-04-04 10:43:39 +0100

    home-image component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SitelogoComponent } from './sitelogo/sitelogo.component';
+import { HomeImageComponent } from './home-image/home-image.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SitelogoComponent
+    SitelogoComponent,
+    HomeImageComponent
   ],
   imports: [
     BrowserModule,
